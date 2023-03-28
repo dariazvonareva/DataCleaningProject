3712271d86325caa397ed49b1750f502811c2fc2
Darja Zvonareva <zvonarevada1@gmail.com>
2023-03-28 16:09:13 +0300

    Added new function

--- Data/outliers_lib/find_outliers.py
+++ Data/outliers_lib/find_outliers.py
@@ -59,3 +59,27 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
+
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Находит выбросы в данных, используя метод межквартильного размаха. 
+    Классический метод модифицирован путем добавления:
+    * в качестве верхней и нижней границ выбираются квантили определенного уровня
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): количество межквартильных размахов в левую сторону распределения. По умолчанию 1.
+        right (float, optional): количество межквартильных размахов в правую сторону распределения. По умолчанию 99.
+        
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
+
